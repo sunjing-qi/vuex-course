e64d8281ce2afdadc7d36bec4bd3d623a1a407e3
sunjing-qi <940676837@qq.com>
2019-08-24 16:25:46 +0800

    'lesson-2'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
-    <p>App.vue: {{count}}</p>
-    <p>app-vue:{{todos}}</p>
-    <Count />
+    <!-- <p>App.vue: {{count}}</p> -->
+    <!-- <p>app-vue:{{todos}}</p> -->
+    <p>App.vue: {{getCount}}</p>
+    <hr />
+    <p>{{completedTodos}}</p>
+    <p>{{completedTodosCount}}</p>
+    <p>{{getTodosById(1)}}</p>
+    <!-- <Count /> -->
   </div>
 </template>
 
 <script>
 import Count from "./components/Count";
 // {}是解构，如果store.js里面没有default，则必须有{}
-import { mapState } from "vuex";
+import { mapState, mapGetters } from "vuex";
 
 export default {
   name: "app",
@@ -32,7 +37,29 @@ export default {
   //   todos:state=>state.todos
   // })
   // 写法3
-  computed: mapState(["count", "todos"])
+  // computed: mapState(["count", "todos"])
+  // 这里面写的方法名不用和store.js里一样
+  // computed: {
+  //   count() {
+  //     return this.$store.getters.getCount;
+  //   },
+  //   completedTodos() {
+  //     return this.$store.getters.completedTodos;
+  //   },
+  //   completedTodosCount() {
+  //     return this.$store.getters.completedTodosCount;
+  //   },
+  //   getTodosById() {
+  //     return this.$store.getters.getTodosById;
+  //   }
+  // },
+  // 这里的名字必须和store.js里面一样
+  computed: mapGetters([
+    "getCount",
+    "completedTodos",
+    "completedTodosCount",
+    "getTodosById"
+  ])
 };
 </script>
 
--- src/store.js
+++ src/store.js
@@ -10,13 +10,35 @@ export default new Vuex.Store({
     todos: [{
       id: 1, title: "todo item1", completed: false
     }, {
-      id: 2, title: "todo item2", completed: false
+      id: 2, title: "todo item2", completed: true
     }, {
-      id: 3, title: "todo item3", completed: false
+      id: 3, title: "todo item3", completed: true
     }]
   },
   getters: {
-
+    // 并没有改变属性的值
+    getCount: state => ++state.count,
+    // count(state){
+    // return ++state.count
+    // }
+    completedTodos: state => state.todos.filter(todo => todo.completed),
+    // completedTodos: function (state) {
+    //   return state.todos.filter(function (todo) {
+    //      return todo.completed;
+    //   })
+    // }
+    completedTodosCount: (state, getters) => getters.completedTodos.length,
+    // completedTodosCount: function (state, getters) {
+    //   return getters.completedTodos.length;
+    // }
+    getTodosById: state => id => state.todos.find(todo => todo.id == id)
+    // getTodosById: function (state) {
+    //   (function(id) {
+    //     return state.todos.find(function (todo) {
+    //       return todo.id = id;
+    //     })
+    //   })(id)
+    // }
   },
   mutations: {
 
