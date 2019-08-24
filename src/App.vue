a559d026b791867ad4912f512b208f0006a0246f
sunjing-qi <940676837@qq.com>
2019-08-24 17:53:27 +0800

    'lesson-4'

--- src/App.vue
+++ src/App.vue
@@ -4,37 +4,52 @@
     <!-- <button @click="decrement({amount:2})">-</button>
     <span>{{getCount}}</span>
     <button @click="increment">+</button>-->
-    <button @click="decrementCount({amount:2})">-</button>
+    <!-- <button @click="decrementCount({amount:2})">-</button>
     <span>{{getCount}}</span>
-    <button @click="incrementCount">+</button>
+    <button @click="incrementCount">+</button>-->
+    <button @click="decrementCountAsync({amount:2})">-</button>
+    <span>{{getCount}}</span>
+    <button @click="incrementCountAsync">+</button>
+    <hr />
+    <p>{{completedTodos}}</p>
+    <button @click="fetchDataAsync">fetchData</button>
+    <!-- <button @click="fetchData">fetchData</button> -->
   </div>
 </template>
 
 <script>
 import Count from "./components/Count";
 // {}是解构，如果store.js里面没有default，则必须有{}
-import { mapState, mapGetters, mapMutations } from "vuex";
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 
 export default {
   name: "app",
   components: { Count },
-
   computed: mapGetters([
     "getCount",
     "completedTodos",
     "completedTodosCount",
     "getTodosById"
   ]),
+  methods: mapActions([
+    "incrementCountAsync",
+    "decrementCountAsync",
+    "fetchDataAsync"
+  ])
   // methods: {
-  //   increment() {
-  //     // 调用mutation里的incrementCount
-  //     this.$store.commit("incrementCount");
+  //   incrementCount() {
+  //     // 触发actions
+  //     this.$store.dispatch("incrementCountAsync");
   //   },
-  //   decrement(n) {
-  //     this.$store.commit("decrementCount", n);
+  //   decrementCount(n) {
+  //     // 触发actions
+  //     this.$store.dispatch("decrementCountAsync", n);
+  //   },
+  //   fetchData() {
+  //     // 触发actions
+  //     this.$store.dispatch("fetchDataAsync");
   //   }
   // }
-  methods: mapMutations(["incrementCount", "decrementCount"])
 };
 </script>
 
--- src/store.js
+++ src/store.js
@@ -23,10 +23,27 @@ export default new Vuex.Store({
     getTodosById: state => id => state.todos.find(todo => todo.id == id)
   },
   mutations: {
+    // 改变
     incrementCount: state => state.count++,
-    decrementCount: (state, number) => state.count -= number.amount
+    decrementCount: (state, number) => state.count -= number.amount,
+    setTodos: (state, todos) => state.todos = todos
   },
   actions: {
-
+    incrementCountAsync: ({ commit }) => {
+      // incrementCountAsync: context => {
+      setTimeout(() => {
+        // context/* =this.$store*/.commit("incrementCount");
+        commit("incrementCount");
+      }, 2000);
+    },
+    decrementCountAsync: (context, number) => {
+      setTimeout(() => {
+        context/* =this.$store*/.commit("decrementCount", number);
+      }, 1000);
+    },
+    async fetchDataAsync(context) {
+      const response = await axios.get("http://jsonplaceholder.typicode.com/todos");
+      context.commit("setTodos", response.data);
+    }
   }
 })
