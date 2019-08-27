237dc58488ee6ee73d05ed4ba8709cd138ed82a5
sunjing-qi <940676837@qq.com>
2019-08-27 17:37:55 +0800

    'demo2'

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    筛选:
+    <select v-model="count" @change="filterTodos(count)">
+      <option v-for="(value,index) in values" :key="index">{{value}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "FilterTodos",
+  data() {
+    return {
+      values: [200, 100, 50, 20, 10, 5],
+      count: 200
+    };
+  },
+  methods: mapActions(["filterTodos"])
+};
+</script>
+
+<style scoped>
+select {
+  margin-top: 20px;
+  padding: 6px;
+  border: 1px solid #41b883;
+}
+</style>
---
+++ src/store/modules/todos.js
@@ -0,0 +1,45 @@
+const state = {
+    todos: []
+};
+const getters = {
+    getAllTodos: state => state.todos
+};
+const mutations = {
+    setTodos: (state, todos) => state.todos = todos,
+    newTodo: (state, todo) => state.todos.unshift(todo),
+    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id != id),
+    updatedTodo: (state, updateTodo) => {
+        const index = state.todos.findIndex(todo => todo.id == updateTodo.id);
+        if (index != -1) {
+            state.todos.splice(index, 1, updateTodo);
+        }
+    }
+};
+const actions = {
+    async fetchTodos({ commit }) {
+        const response = await axios.get("http://jsonplaceholder.typicode.com/todos");
+        commit("setTodos", response.data);
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post("http://jsonplaceholder.typicode.com/todos", {
+            title, completed: false
+        });
+        commit("newTodo", response.data);
+    },
+    async deleteTodo({ commit }, id) {
+        await axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`);
+        commit("removeTodo", id);
+    },
+    async filterTodos({ commit }, count) {
+        const response = await axios.get(`http://jsonplaceholder.typicode.com/todos?_limit=${count}`);
+        commit("setTodos", response.data);
+    },
+    async updateTodo({ commit }, todo) {
+        const response = await axios.put(`http://jsonplaceholder.typicode.com/todos/${todo.id}`, todo);
+        commit("updatedTodo", response.data);
+    }
+};
+
+export default {
+    state, getters, mutations, actions
+}
